80ffc55d45abe67a4fb72022382dcc9d8ecaede7
goody-1 <atundeg@gmail.com>
2024-10-04 16:48:18 +0100

    troubleshoot checker failure

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = async (firstName, lastName, fileName) => {
-  // Use Promise.allSettled to handle both promises and wait for them to complete
-  const results = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
-
-  // Return the array with status and value/reason
-  return results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  }));
-};
+const handleProfileSignup = (firstName, lastName, fileName) => Promise.allSettled(
+  [signUpUser(firstName, lastName), uploadPhoto(fileName)],
+);
 
 export default handleProfileSignup;
